52fa6a915c7b1d3891729bd055fb788c99e692b9
Gonzalo <gonzalovescovi@gmail.com>
2018-10-24 15:16:44 +0200

    Created vector

--- positions.cpp
+++ positions.cpp
@@ -4,13 +4,25 @@
 #include "import.hpp"
 #include "positions.hpp"
 
-positions::positions(const double& a)
+positions::positions(const double& a, const doulbe& b)
 {
     int n;
     dx = a;
+    dy = b;
+
     n = 1 / dx;
-    for (int j = 0; j <= n; j++)
+    m = 1/ dy;
+    Xu.push_back(0);
+    for (int j = 0; j < n; j++)
     {
         Xp.push_back(j * dx);
+        Xu.push_back((j + 1/2) * dx);
     }
+    Xp.push_back(n * dx); Xu.push_back(n * dx);
+    for (j = 0; j < m; j++)
+    {
+        Yp.push_back(j * dx);
+        Yv.push_back((j + 1/2) * dx);
+    }
+    Xp.push_back(m * dx); Xu.push_back(m * dx);
 }
--- positions.hpp
+++ positions.hpp
@@ -7,10 +7,10 @@
 #define INCLUDE_POS
 class positions{
     private:
-        double dx;
-        std::vector<double> Xp, Yp, Xu;
+        double dx,dy;
+        std::vector<double> Xp, Yp, Xu, Yv;
     public:
-        positions(const double& a);
+        positions(const double& a, const double& b);
         std::vector<double> get_Xp();
         std::vector<double> get_Yp();
         std::vector<double> get_Yu();
